d8b213a123d0d5e36f36f3d7956f4970dde77f82
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-03-09 19:03:40 +0530

    searching element in array using macro,constant,funtion pointers

---
+++ search element in array.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#define max_size 10//macro 
+int i,l;
+void search(const int ,const int *,const int);
+int main()
+{
+   int n,m;
+   printf("enter the size of array:");
+   scanf("%d",&n);
+   int a[max_size];
+   printf("enter the elements:\n");
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&a[i]);
+   }
+   printf("enter the element to be searched:");
+   scanf("%d",&m);
+   void (*fp)(const int ,const int *,const int);
+   fp=search;
+   fp(n,a,m);
+   return 0;
+}
+void search(const int n,const int *a,const int m){
+   for(i=0;i<n;i++)
+   {
+      if(m==a[i]){
+         l=1;
+         break;
+      }
+   }
+   if(l==1)
+   {
+      printf("%d is present in the array",m);
+   } 
+   else 
+   {
+      printf("%d is not present in the array",m);
+   }
+}
